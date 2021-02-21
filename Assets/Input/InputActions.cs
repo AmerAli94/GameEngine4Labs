b01b1075a349a07d06158bcbc6b7eb4fd97121bf
Amer Ali Mohammed <ameralizee94@gmail.com>
2021-02-21 17:34:59 -0500

    initial commit

---
+++ Assets/Input/InputActions.cs
@@ -0,0 +1,322 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Input/InputActions.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @InputActions : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @InputActions()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputActions"",
+    ""maps"": [
+        {
+            ""name"": ""Third Person"",
+            ""id"": ""666b9195-e64a-4936-ae3c-2f30c13e15b2"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""fff7fe9f-e601-472c-a47a-eb5f146dc314"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""aa30469a-ab90-406e-98cc-b00612e08d09"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": ""Press""
+                },
+                {
+                    ""name"": ""Fire"",
+                    ""type"": ""Button"",
+                    ""id"": ""5d876e0f-2f69-4856-b982-88d21f9ebc95"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Run"",
+                    ""type"": ""Button"",
+                    ""id"": ""dc165821-b1b6-4782-88fb-51d054ec5f71"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": ""Press(behavior=2)""
+                },
+                {
+                    ""name"": ""Look"",
+                    ""type"": ""Value"",
+                    ""id"": ""2bd2ba4b-e91c-48b3-8dac-124e4f60fca7"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": ""NormalizeVector2"",
+                    ""interactions"": ""Press(behavior=2)""
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""780cde6b-f704-4a22-9da0-d217634f15f3"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""ffebea2f-977e-42e4-b3b7-e41c79c42807"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""f84c44d3-87be-476f-a1cb-751502e3ee55"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""e3526532-4325-40c9-b236-ca6949de90e0"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""173e02c6-52b7-4a71-ad5d-51351f35b374"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""222b62da-f893-4c69-a1eb-562dff70226e"",
+                    ""path"": """",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Fire"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""994f0ced-217e-4d8e-adfd-1a1e6053779e"",
+                    ""path"": ""<Keyboard>/shift"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Run"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""1d99db93-0d22-4f1d-8379-9a95f0b38487"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""1964dd42-cec8-4aa4-b4bb-3275cf7c698f"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Keyboard"",
+            ""bindingGroup"": ""Keyboard"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<VirtualMouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // Third Person
+        m_ThirdPerson = asset.FindActionMap("Third Person", throwIfNotFound: true);
+        m_ThirdPerson_Movement = m_ThirdPerson.FindAction("Movement", throwIfNotFound: true);
+        m_ThirdPerson_Jump = m_ThirdPerson.FindAction("Jump", throwIfNotFound: true);
+        m_ThirdPerson_Fire = m_ThirdPerson.FindAction("Fire", throwIfNotFound: true);
+        m_ThirdPerson_Run = m_ThirdPerson.FindAction("Run", throwIfNotFound: true);
+        m_ThirdPerson_Look = m_ThirdPerson.FindAction("Look", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Third Person
+    private readonly InputActionMap m_ThirdPerson;
+    private IThirdPersonActions m_ThirdPersonActionsCallbackInterface;
+    private readonly InputAction m_ThirdPerson_Movement;
+    private readonly InputAction m_ThirdPerson_Jump;
+    private readonly InputAction m_ThirdPerson_Fire;
+    private readonly InputAction m_ThirdPerson_Run;
+    private readonly InputAction m_ThirdPerson_Look;
+    public struct ThirdPersonActions
+    {
+        private @InputActions m_Wrapper;
+        public ThirdPersonActions(@InputActions wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_ThirdPerson_Movement;
+        public InputAction @Jump => m_Wrapper.m_ThirdPerson_Jump;
+        public InputAction @Fire => m_Wrapper.m_ThirdPerson_Fire;
+        public InputAction @Run => m_Wrapper.m_ThirdPerson_Run;
+        public InputAction @Look => m_Wrapper.m_ThirdPerson_Look;
+        public InputActionMap Get() { return m_Wrapper.m_ThirdPerson; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(ThirdPersonActions set) { return set.Get(); }
+        public void SetCallbacks(IThirdPersonActions instance)
+        {
+            if (m_Wrapper.m_ThirdPersonActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_ThirdPersonActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_ThirdPersonActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_ThirdPersonActionsCallbackInterface.OnMovement;
+                @Jump.started -= m_Wrapper.m_ThirdPersonActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_ThirdPersonActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_ThirdPersonActionsCallbackInterface.OnJump;
+                @Fire.started -= m_Wrapper.m_ThirdPersonActionsCallbackInterface.OnFire;
+                @Fire.performed -= m_Wrapper.m_ThirdPersonActionsCallbackInterface.OnFire;
+                @Fire.canceled -= m_Wrapper.m_ThirdPersonActionsCallbackInterface.OnFire;
+                @Run.started -= m_Wrapper.m_ThirdPersonActionsCallbackInterface.OnRun;
+                @Run.performed -= m_Wrapper.m_ThirdPersonActionsCallbackInterface.OnRun;
+                @Run.canceled -= m_Wrapper.m_ThirdPersonActionsCallbackInterface.OnRun;
+                @Look.started -= m_Wrapper.m_ThirdPersonActionsCallbackInterface.OnLook;
+                @Look.performed -= m_Wrapper.m_ThirdPersonActionsCallbackInterface.OnLook;
+                @Look.canceled -= m_Wrapper.m_ThirdPersonActionsCallbackInterface.OnLook;
+            }
+            m_Wrapper.m_ThirdPersonActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @Fire.started += instance.OnFire;
+                @Fire.performed += instance.OnFire;
+                @Fire.canceled += instance.OnFire;
+                @Run.started += instance.OnRun;
+                @Run.performed += instance.OnRun;
+                @Run.canceled += instance.OnRun;
+                @Look.started += instance.OnLook;
+                @Look.performed += instance.OnLook;
+                @Look.canceled += instance.OnLook;
+            }
+        }
+    }
+    public ThirdPersonActions @ThirdPerson => new ThirdPersonActions(this);
+    private int m_KeyboardSchemeIndex = -1;
+    public InputControlScheme KeyboardScheme
+    {
+        get
+        {
+            if (m_KeyboardSchemeIndex == -1) m_KeyboardSchemeIndex = asset.FindControlSchemeIndex("Keyboard");
+            return asset.controlSchemes[m_KeyboardSchemeIndex];
+        }
+    }
+    public interface IThirdPersonActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+        void OnFire(InputAction.CallbackContext context);
+        void OnRun(InputAction.CallbackContext context);
+        void OnLook(InputAction.CallbackContext context);
+    }
+
+    internal class CallbackContext
+    {
+    }
+}
---
+++ Assets/Scripts/Character/crosshairscript.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crosshairscript : MonoBehaviour
+{
+    public Vector2 MouseSensitivity = Vector2.zero;
+    public bool Inverted = false;
+    public Vector2 CurrentMousePosition { get; private set; }
+
+    private Vector2 CrosshairStartingPosition;
+
+    [SerializeField, Range(0.0f,1.0f)]
+    private float HorizontalPercentageConstrain;
+    [SerializeField, Range(0.0f, 1.0f)]
+    private float VerticalPercentageConstrain;
+    private float HorizontalConstrain;
+    private float VerticalConstrain;
+
+
+    private Vector2 CurrentLookDelta = Vector2.zero;
+
+    private float MaxHorizontalConstrainValue;
+    private float MaxVerticalConstrainValue;
+
+    private float MinHorizontalConstrainValue;
+    private float MinVerticalConstrainValue;
+
+    private InputActions input_Actions;
+
+    
+
+    private void Awake()
+    {
+        input_Actions = new InputActions();
+    }
+
+    void Start()
+    {
+        if (GameManager.Instance.CursorActive)
+            AppEvents.Invoke_MouseCursorEnable(false);
+
+        CrosshairStartingPosition = new Vector2(Screen.width / 2f, Screen.height / 2f);
+
+        HorizontalConstrain = (Screen.width * HorizontalPercentageConstrain) / 2f;
+
+        MinHorizontalConstrainValue = -(Screen.width / 2) + HorizontalConstrain;
+        MaxHorizontalConstrainValue = (Screen.width / 2) - HorizontalConstrain;
+
+        VerticalConstrain = (Screen.height * VerticalPercentageConstrain) * 2f;
+        MinVerticalConstrainValue = -(Screen.width / 2) + VerticalConstrain;
+        MaxVerticalConstrainValue = (Screen.width / 2) - VerticalConstrain;
+
+    }
+
+    void Update()
+    {
+        float crosshairXPosition = CrosshairStartingPosition.x + CurrentLookDelta.x;
+
+        float crosshairYPosition = Inverted
+            ? CrosshairStartingPosition.y - CurrentLookDelta.y
+            : CrosshairStartingPosition.y + CurrentLookDelta.y;
+
+        CurrentMousePosition = new Vector2(crosshairXPosition, crosshairYPosition);
+        
+        transform.position = CurrentMousePosition;
+
+    }
+
+    private void OnLook(UnityEngine.InputSystem.InputAction.CallbackContext delta)
+    {
+        Vector2 mouseDelta = delta.ReadValue<Vector2>();
+        CurrentLookDelta.x += mouseDelta.x * MouseSensitivity.x;
+        if (CurrentLookDelta.x >= MaxHorizontalConstrainValue || CurrentLookDelta.x <= MinHorizontalConstrainValue)
+        {
+            CurrentLookDelta.x -= mouseDelta.x * MouseSensitivity.x;
+        }
+
+        CurrentLookDelta.y += mouseDelta.y * MouseSensitivity.y;
+        if (CurrentLookDelta.y >= MaxVerticalConstrainValue || CurrentLookDelta.y <= MinVerticalConstrainValue)
+        {
+            CurrentLookDelta.y -= mouseDelta.y * MouseSensitivity.y;
+        }
+    }
+
+
+    private void OnEnable()
+    {
+        input_Actions.Enable();
+        input_Actions.ThirdPerson.Look.performed += OnLook;
+
+    }
+
+
+    private void OnDisable()
+    {
+        input_Actions.Disable();
+        input_Actions.ThirdPerson.Look.performed -= OnLook;
+
+    }
+}
